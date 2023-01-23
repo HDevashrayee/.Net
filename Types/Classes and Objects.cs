1b603e21f7b3e0c460eca1225bd3ecdb54da3b66
HDevashrayee <113212932+HDevashrayee@users.noreply.github.com>
2023-01-23 18:05:48 +0530

    commitr

--- Types/Classes and Objects.cs
+++ Types/Classes and Objects.cs
@@ -6,5 +6,12 @@ namespace Types
 {
     internal class Classes_and_Objects
     {
+        public class Point
+        {
+            public int X { get; }
+            public int Y { get; }
+
+            public Point(int x, int y) => (X, Y) = (x, y);
+        }
     }
 }
